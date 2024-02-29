66666a4ecfdb6145a7053b14b8101d3dab4228cf
Lars Kruse <lakruzz@inc-inc.dk>
2024-02-29 18:49:34 +0000

    postcreate

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash --login
 
 bundle update
-bash -l -c "rvm use `cat .ruby-version`"
+rvm use 3.2.3
 bundle install
 
 npm install cspell --global
 
+git config --global --add safe.directory /workspaces/thetechcollective.dev
+
 git config --local --get include.path | grep -e ../.gitconfig || git config --local --add include.path ../.gitconfig
+
 
 
